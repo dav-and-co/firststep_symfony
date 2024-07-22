637ababd8f408c6a99d5b0878a2d5cf0689ff5ed
dav-and-co <claude.daviatte@lapiscine.pro>
2024-07-22 14:13:06 +0200

    fisrt push

---
+++ src/Controller/indexController.php
@@ -0,0 +1,22 @@
+<?php
+
+// http://localhost/piscine-2207-symfonyBase/public/
+
+
+// definition du chemin de fichier concerné (app (à la place de sec) puis répertoire controller
+namespace App\Controller;
+// permet d'utiliser le module AbstractController de symfony
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+// permet d'utiliser le module Route de symfony
+use Symfony\Component\Routing\Attribute\Route;
+
+//création de la class du controller
+class indexController extends AbstractController
+
+{
+    // création de l'affichage de l'adresse intenet et renvoi vers ce controller
+    #[Route('/', name: 'index')]
+    public function index(){
+        var_dump(value: "hello Tous" );die;
+    }
+ }
